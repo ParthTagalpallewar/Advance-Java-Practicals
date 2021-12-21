1aef8733b83a62d6160747376470e91ab3d83143
Parth Tagalpallewar <parthtagalpallewar123@gamil.com>
2021-12-21 11:54:56 +0530

    practical no 10 is completed

---
+++ Practical_10/Test.java
@@ -0,0 +1,9 @@
+import Bean.Employee; 
+
+public class Test{  
+    public static void main(String args[]){  
+        Employee e = new Employee();//object is created  
+        e.setName("Arjun");//setting value to the object  
+        System.out.println(e.getName());  
+    }
+}  
