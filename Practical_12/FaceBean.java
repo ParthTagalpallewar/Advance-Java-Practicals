e61349f8d049454d888809fe0cf71678fac9c26e
Parth Tagalpallewar <parthtagalpallewar123@gamil.com>
2021-12-21 13:35:52 +0530

    completed practical no 12

--- Practical_12/FaceBean.java
+++ Practical_12/FaceBean.java
@@ -2,17 +2,19 @@ import java.beans.*;
 
 public class FaceBean implements java.io.Serializable {
     private int mMouthWidth = 90;
-    
+
     private PropertyChangeSupport mPcs = new PropertyChangeSupport(this);
+    private VetoableChangeSupport mVcs = new VetoableChangeSupport(this);
 
     public int getMouthWidth() {
         return mMouthWidth;
     }
     
-    public void setMouthWidth(int mw) {
+    public void setMouthWidth(int mw) throws PropertyVetoException {
         int oldMouthWidth = mMouthWidth;
         mMouthWidth = mw;
         mPcs.firePropertyChange("mouthWidth", oldMouthWidth, mw);
+        mVcs.fireVetoableChange("mouthWidth", oldMouthWidth, mw);
     }
 
     public void addPropertyChangeListener(PropertyChangeListener listener) {
@@ -21,5 +23,13 @@ public class FaceBean implements java.io.Serializable {
     
     public void removePropertyChangeListener(PropertyChangeListener listener) {
         mPcs.removePropertyChangeListener(listener);
+    }
+
+    public void addVetoableChangeListener(VetoableChangeListener listener) {
+        mVcs.addVetoableChangeListener(listener);
+    }
+
+    public void removeVetoableChangeListener(VetoableChangeListener listener) {
+        mVcs.removeVetoableChangeListener(listener);
     }
 }
--- Practical_12/Practical_12.java
+++ Practical_12/Practical_12.java
@@ -4,6 +4,7 @@ import java.awt.Graphics;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
+import java.beans.VetoableChangeListener;
 import java.io.Serializable;
 import java.util.Arrays;
 import javax.swing.JComponent;
@@ -21,9 +22,25 @@ public class Practical_12 implements Serializable{
                 " New value "+ event.getNewValue());
         });
 
-        s.setMouthWidth(20);
+        s.addVetoableChangeListener(event ->{
+            
+            System.out.println(
+                "Vetoreble Property changed "+event.getPropertyName() + 
+                " old value "+event.getOldValue() + 
+                " New value "+ event.getNewValue());
+        });
 
-        s.setMouthWidth(30);
+        VetoableChangeListener
+
+        try {
+            s.setMouthWidth(20);
+
+            s.setMouthWidth(30);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        
         
     }
     
